9d28c38e58d6d31d031f7c3798324e96721d65d7
ys3197 <43221315+ys3197@users.noreply.github.com>
2023-06-28 17:45:26 -0400

    Add comments to the functions

--- Kasheesh.py
+++ Kasheesh.py
@@ -30,17 +30,17 @@ def show_records_userid(id):
     # convert into pandas dataframe and combine two tables
     df_return = pd.DataFrame(sql_query_return, columns = ['user_id', 'merchant_type_code', 'amount_cents', 'datetime', 'transaction_type'])
     df_purchase = pd.DataFrame(sql_query_purchase, columns = ['user_id', 'merchant_type_code', 'amount_cents', 'datetime', 'transaction_type'])
-    df = pd.concat([df_return, df_return], ignore_index = True)
+    df_all_transaction = pd.concat([df_return, df_return], ignore_index = True)
 
     # ouput and deal with errors
-    if len(df) > 0:
+    if len(df_all_transaction) > 0:
         print("Data grabbed successfully")
-        return Response(df.to_json(orient="records"), status=200, mimetype='application/json')
+        return Response(df_all_transaction.to_json(orient="records"), status=200, mimetype='application/json')
     else:
         return "User not found", 404
 
 @app.route('/merchanttypecode/<int:merchant_type>')
-def show_records_merchant(merchant_type):
+def show_net_purchase(merchant_type):
     """
     Function to show the daily total purchases net of returns given a merchant_type_code
     """
@@ -64,13 +64,13 @@ def show_records_merchant(merchant_type):
                                '''.format(merchant_type), db)
     # Merge the tables and get the net purchase of return
     df_return = pd.DataFrame(sql_query_return, columns = ['merchant_type_code', 'net_amount_return_in_dollars', 'date'])
-    df = df_purchase.merge(df_return, how='outer', on=['merchant_type_code', 'date']).fillna(0)
-    df['net_amount_in_dollars'] = df['net_amount_purchase_in_dollars'] - df['net_amount_return_in_dollars']
+    df_net_purchase = df_purchase.merge(df_return, how='outer', on=['merchant_type_code', 'date']).fillna(0)
+    df_net_purchase['net_amount_in_dollars'] = df_net_purchase['net_amount_purchase_in_dollars'] - df_net_purchase['net_amount_return_in_dollars']
 
     # ouput and deal with errors
-    if len(df) > 0:
+    if len(df_net_purchase) > 0:
         print("Data grabbed successfully")
-        return Response(df[['merchant_type_code', 'net_amount_in_dollars', 'date']].to_json(orient="records"), status=200, mimetype='application/json')
+        return Response(df_net_purchase[['merchant_type_code', 'net_amount_in_dollars', 'date']].to_json(orient="records"), status=200, mimetype='application/json')
 
     else:
         return "Merchant not found", 404
